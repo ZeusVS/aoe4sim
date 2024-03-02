01ba4e54beadfa6cf4705aa83b6eff539d9a6e2c
ZeusVS <zinovansummeren@gmail.com>
2024-03-02 23:23:40 +0100

    Ability to add multiple units and get their stats

--- getData.js
+++ getData.js
@@ -2,19 +2,59 @@ const path = require('path')
 
 // The url to the aoe4world repo's location of all civilizations
 const civURL = 'https://github.com/aoe4world/data/tree/main/civilizations';
+// The url to the aoe4world repo's location of all units
+const unitURL = 'https://github.com/aoe4world/data/tree/main/units';
 
 // Get all the current possible civilizations in the game
 async function getCivs() {
     try {
-        const response = await fetch(civURL,);
+        const response = await fetch(civURL);
         const json = await response.json();
         const rawCivs = json.payload.tree.items;
         const civs = [];
         for (const civ of rawCivs) {
             civs.push(path.parse(civ.name).name);
         }
-        console.log(civs);
         return civs;
+    } catch (err) {
+        console.log(err.message);
+        // What else to do on fail?
+    }
+}
+
+// Get all the stats for the given unit and civ
+async function getStats(civ, unit) {
+    try {
+        const response = await fetch(`${unitURL}/${civ}/${unit}.json?raw=true`);
+        const rawStats = await response.json();
+        const stats = {
+            id: rawStats.id,
+            name: rawStats.name,
+            classes: rawStats.classes,
+            hitpoints: rawStats.hitpoints,
+            weapons: rawStats.weapons,
+            armor: rawStats.armor,
+            movement: rawStats.movement,
+        }
+        console.log(stats)
+        return stats;
+    } catch (err) {
+        console.log(err.message);
+        // What else to do on fail?
+    }
+}
+
+// Get all the current possible units for the given civ
+async function getUnits(civ) {
+    try {
+        const response = await fetch(`${unitURL}/${civ}`);
+        const json = await response.json();
+        const rawUnits = json.payload.tree.items;
+        const units = [];
+        for (const unit of rawUnits) {
+            units.push(path.parse(unit.name).name);
+        }
+        return units;
     } catch (err) {
         console.log(err.message);
         // What else to do on fail?
@@ -23,4 +63,6 @@ async function getCivs() {
 
 module.exports = {
     getCivs,
+    getUnits,
+    getStats,
 }
--- input.js
+++ input.js
@@ -1,23 +1,39 @@
-const { getCivs } = require('./getData.js');
-
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+const { getCivs, getUnits, getStats } = require('./getData.js');
 
 // Wrap `readline.question` in a Promise so that we can use it in an async function
-async function inputQuestion(question, validAnswers) {
+async function inputAmount(question) {
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
     let answer;
     do {
         answer = await new Promise((resolve, reject) => {
-            readline.question(`Pick ${question}, possible choices: ${validAnswers}\n`, (input) => {
+            readline.question(question, (input) => {
+                resolve(input);
+            });
+        });
+    } while (answer != parseInt(answer) || answer <= 0);
+    readline.close();
+    return answer;
+}
+
+async function inputQuestion(question, validAnswers) {
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    let answer;
+    do {
+        answer = await new Promise((resolve, reject) => {
+            readline.question(question, (input) => {
                 resolve(input);
             });
         });
     } while (!validAnswers.includes(answer));
+    readline.close();
     return answer;
 }
-
 
 class inputClass {
     constructor(civ1, army1, civ2, army2) {
@@ -25,21 +41,58 @@ class inputClass {
         this.army1 = army1;
         this.civ2 = civ2;
         this.army2 = army2;
-        readline.close();
+    }
+}
+
+class Army {
+    constructor() {
+
     }
 }
 
 async function getInput() {
-    // Get possible civs
-    possibleCivs = await getCivs();
-    // Get possible units for the chosen civ
+    // Get all currently existing civs in the game
+    const possibleCivs = await getCivs();
+    const army1 = {}
+    const army2 = {}
 
-    // Get user input
-    const civ1 = await inputQuestion('player 1 civ', possibleCivs);
-    const army1 = await inputQuestion('player 1 army', ['Greek']);
-    const civ2 = await inputQuestion('player 2 civ', possibleCivs);
-    const army2 = await inputQuestion('player 2 army', ['Greek']);
-    // Create inputObject out of the responses we got and return t
+    // Get player 1 civ choice
+    const civ1 = await inputQuestion(`Give civ for player 1\n${possibleCivs}\n`, possibleCivs);
+    // Get all currently existing units for the chosen civ
+    const possibleUnits1 = await getUnits(civ1);
+    
+    // Get player 1 unit choice
+    while (true) {
+        const unit = await inputQuestion(`Add unit to player 1 army\n${possibleUnits1}\n`, possibleUnits1);
+        const unitStats = await getStats(civ1, unit);
+        const unitQt = await inputAmount(`Amount of ${unit} units to place\n`);
+        army1[unitStats] = unitQt;
+        console.log(`Added ${unitQt} unit(s) of ${unit} to the army of player 1`);
+        const progress = await inputQuestion('(A)dd another unit or (C)ontinue?\n', ['a', 'A', 'c', 'C']);
+        if (progress === 'c' || progress === 'C') {
+            break;
+        }
+    }
+
+    // Get player 2 civ choice
+    const civ2 = await inputQuestion(`Give civ for player 2\n${possibleCivs}\n`, possibleCivs);
+    // Get all currently existing units for the chosen civ
+    const possibleUnits2 = await getUnits(civ2);
+
+    // Get player 2 unit choice
+    while (true) {
+        const unit = await inputQuestion(`Add unit to player 2 army\n${possibleUnits2}\n`, possibleUnits2);
+        const unitStats = await getStats(civ2, unit);
+        const unitQt = await inputAmount(`Number of ${unit} units to place\n`);
+        army2[unitStats] = unitQt;
+        console.log(`Added ${unitQt} unit(s) of ${unit} to the army of player 2`);
+        const progress = await inputQuestion('(A)dd another unit or (C)ontinue?\n', ['a', 'A', 'c', 'C']);
+        if (progress === 'c' || progress === 'C') {
+            break;
+        }
+    }
+
+    // Create inputObject out of the responses we got and return it
     const inputObject = new inputClass(civ1, army1, civ2, army2)
     return inputObject;
 }
